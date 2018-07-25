575ef1df4cb271ef8b218d73d0ddabc03c25b3fe
Gustavo Santos <gustavosms@localhost.localdomain>
2018-07-25 10:25:47 -0300

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,86 @@
 /*
  * Create a list that holds all of your cards
  */
+var icons = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
 
+const CARD_STATE = { "UNTAPPED": "card", "TAPPED": "card open show", "MATCHED": "card match show" }
+
+const generateShuffledCards = () => {
+    var cards = [];
+    icons.forEach((icon, index) => {
+        var card = { id: index, icon: "fa " + icon, state: "UNTAPPED" };
+        cards = cards.concat(card, Object.assign({}, card));
+    });
+
+    cards = shuffle(cards);
+    cards.map((card, index) => {
+        return Object.assign(card, { id: index })
+    });
+
+    return cards;
+}
+
+const isSameCard = (c1, c2) => {
+    return c1.id == c2.id;
+}
+
+const isComplementaryCard = (c1, c2) => {
+    return !isSameCard(c1, c2) && c1.icon === c2.icon;
+}
+
+const changeCardsState = (newState, ...cards) => {
+    cards.forEach((card) => { card.state = newState });
+}
+
+const tapCards = (...cards) => {
+    this.changeCardsState('TAPPED', cards);
+}
+
+const matchCards = (...cards) => {
+    this.changeCardsState('MATCHED', cards);
+}
+
+var app = new Vue({
+    el: '.container',
+    data: {
+      cards: generateShuffledCards(),
+      lastCards: []
+    },
+    methods: {
+        tapCard: (card) => {
+
+            if (this.lastCards.size == 2) {
+                tapCards(this.lastCards);
+                this.lastCards = [];
+            }
+
+            var tappedCard = this.lastCards[0];
+            console.log('Actual pressed card: ', card);
+            if (tappedCard !== undefined) {
+                console.log('Previous pressed card: ' + tappedCard);
+
+                if (isSameCard(card, tappedCard)) {
+                    return;
+                }
+
+                if (isComplementaryCard(card, tappedCard)) {
+                    matchCards(card, tappedCard);
+                } else {
+                    tapCards(card);
+                }
+
+                this.lastCards.push(card);
+
+            } else if (card.state === 'UNTAPPED') {
+                tapCards(card);
+                this.tappedCard = card;
+            }
+        },
+        cardClass: (card) => {
+            return CARD_STATE[card.state];
+        }
+    }
+})
 
 /*
  * Display the cards on the page
