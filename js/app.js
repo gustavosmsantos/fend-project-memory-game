6a0d3e69117093b7fb5ebcf9b8bedcf351872b7a
Gustavo Santos <gustavosms@localhost.localdomain>
2018-07-28 01:44:43 -0300

    fix: same card click; flipping cards effect

--- js/app.js
+++ js/app.js
@@ -33,31 +33,33 @@ var app = new Vue({
             if (!this.isSwitching) {
                 var untappedCard = this.lastCard;
                 console.log('Actual pressed card: ', card);
-                if (untappedCard !== undefined) {
-                    console.log('Previous pressed card: ', untappedCard);
-                    if (!isSameCard(card, untappedCard)) {
-                        this.isSwitching = true;
-                        if (isComplementaryCard(card, untappedCard)) {
-                            matchCards(card, untappedCard);
-                            this.isSwitching = false;
-                            this.matchedAmount++;
-                        } else {
-                            untapCards(card);
-                            setTimeout(() => { 
-                                tapCards(card, untappedCard);
+                if (card.state === 'TAPPED' ) {
+                    if (untappedCard !== undefined) {
+                        console.log('Previous pressed card: ', untappedCard);
+                        if (!isSameCard(card, untappedCard)) {
+                            this.isSwitching = true;
+                            if (isComplementaryCard(card, untappedCard)) {
+                                matchCards(card, untappedCard);
                                 this.isSwitching = false;
-                            }, 1000);
-                        }
-                        this.lastCard = undefined;
+                                this.matchedAmount++;
+                            } else {
+                                untapCards(card);
+                                setTimeout(() => { 
+                                    tapCards(card, untappedCard);
+                                    this.isSwitching = false;
+                                }, 1000);
+                            }
+                            this.lastCard = undefined;
 
-                        if (this.endedMatch()) {
-                            this.endTime = Date.now();
+                            if (this.endedMatch()) {
+                                this.endTime = Date.now();
+                            }
                         }
+                        this.moves++;
+                    } else {
+                        untapCards(card);
+                        this.lastCard = card;
                     }
-                    this.moves++;
-                } else if (card.state === 'TAPPED') {
-                    untapCards(card);
-                    this.lastCard = card;
                 }
             }
         },
